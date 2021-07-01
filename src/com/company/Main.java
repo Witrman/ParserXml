291c3a50e3845bea412f553a088a3260d2ff343c
Witrman <Vitr.man95@gmail.com>
2021-07-01 19:01:04 +0500

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,21 +12,23 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-            Set<String> setOfNames = new HashSet<>();
+            SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser(); //для чтения xlm документа используем SAXParser
+            Set<String> setOfNames = new HashSet<>(); //используем для подсчета уникальных значений
             DefaultHandler handler = new DefaultHandler() {
                 @Override
                 public void startElement(String uri, String localName, String qName, Attributes attributes) {
 
-                    if (qName.equalsIgnoreCase("modification")) {
-                            setOfNames.add(attributes.getValue("name"));
-                    }
+                    if (qName.equalsIgnoreCase("modification")) {  //при первом попадании на тэг modification
+                            setOfNames.add(attributes.getValue("name")); //значение атрибута name добавляется в HashSet
+                    }                                                           // здесь могут находится только уникальные значения
                 }
             };
 
             saxParser.parse(args[0], handler);
             System.out.println(setOfNames.size());
 
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Input file name: \"java -jar ParserXml.jar cars.xml\"");
         } catch (Exception e) {
             e.printStackTrace();
         }
