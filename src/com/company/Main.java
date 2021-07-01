c1dc1cd4de6e017fc8d845dbab03e7d37b7bb210
Witrman <Vitr.man95@gmail.com>
2021-07-01 18:28:37 +0500

    use HashSet, change java version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,42 +5,28 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-            HashMap<String, Integer> mapOfNames = new HashMap<>();
+            Set<String> setOfNames = new HashSet<>();
             DefaultHandler handler = new DefaultHandler() {
                 @Override
                 public void startElement(String uri, String localName, String qName, Attributes attributes) {
 
                     if (qName.equalsIgnoreCase("modification")) {
-                        String name = attributes.getValue("name");
-                        if(!mapOfNames.containsKey(name)){
-                            mapOfNames.put(name,1);
-                        }else{
-                            mapOfNames.put(name,mapOfNames.get(name)+1);
-                        }
+                            setOfNames.add(attributes.getValue("name"));
                     }
                 }
             };
-/*
-            if(args[0].equals("")) {
-                System.out.println("Введите название XML файла после команды.");
-                return;
-            }*/
-            String path = "cars.xml";
-            saxParser.parse(path, handler);
-            for (Map.Entry<String, Integer> pair : mapOfNames.entrySet())
-            {
-                String key = pair.getKey();
-                int value = pair.getValue();
-                System.out.println(key + " : " + value);
-            }
+
+            saxParser.parse(args[0], handler);
+            System.out.println(setOfNames.size());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
